d8f4fde0e64d687f0d57f97df29ef952c277a0a4
heehaw1234 <seanlee2577@gmail.com>
2025-05-17 10:54:18 +0800

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
 import './App.css';
 
 const Card = ({ title }) => {
@@ -7,11 +8,13 @@ const Card = ({ title }) => {
   return (
     <div className="card">
       <h2 className="title">{title}</h2>
-      <img
-        className="card_img"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Eo_circle_green_white_checkmark.svg/2048px-Eo_circle_green_white_checkmark.svg.png"
-        alt="Card image"
-      />
+      <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Eo_circle_green_white_checkmark.svg/2048px-Eo_circle_green_white_checkmark.svg.png" className="card_img" target="blank">
+        <img
+          className="card_img"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Eo_circle_green_white_checkmark.svg/2048px-Eo_circle_green_white_checkmark.svg.png"
+          alt="Card image"
+        />
+      </a>
       <button className="likebtn" onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? "✔" : "Like"}
       </button>
@@ -20,12 +23,25 @@ const Card = ({ title }) => {
 };
 
 const Topbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="topbar">
       <div className="noteit">NoteIt</div>
       <div className="auth-buttons">
-        <button className="btn">Sign In</button>
-        <button className="btn">Login</button>
+        <Button className="btn" variant="outlined" onClick={() => setOpen(true)}>Sign Up / Login</Button>
+        <Dialog open={open} onClose={() => setOpen(false)} id="dialog_login">
+          <DialogTitle>Login / Sign up</DialogTitle>
+          <DialogContent className="dialog_content">Email</DialogContent>
+          <input className="login" type="text" placeholder="Enter your email"/>
+          <DialogContent className="dialog_content">Username</DialogContent>
+          <input className="login" type="text" placeholder="Enter a Username"/>
+          <DialogContent className="dialog_content">Password</DialogContent>
+          <input className="login" type="text" placeholder="Enter your Password"/>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)}>Close</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </div>
   );
@@ -46,7 +62,7 @@ const App = () => {
         <Card title="One Punch Man" />
         <Card title="Demon Slayer: Mugen Train" />
         <Card title="Chainsaw Man" />
-        <Card title="HunterXHunter" />
+        <Card title="Hunter X Hunter" />
         <Card title="Big Bang Theory" />
       </div>
     </div>
